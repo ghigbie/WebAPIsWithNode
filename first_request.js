a15dc289f0d04a89ea6dd14879cca0e9d3c9aefc
ghigbie <gehigbie@gmail.com>
2017-02-16 23:45:12 +0000

    working version

--- first_request.js
+++ first_request.js
@@ -1,7 +1,16 @@
 var request = require("request");
 
 request('http://www.google.com', function(error, response, body){
-    if(!error && response.statusCode == 200){
-        console.log(body); //Show the HTML for the google homepage
+    // if(!error && response.statusCode == 200){ //why not ===????
+    //     console.log(body); //Show the HTML for the google homepage
+    // }
+    
+    if(error){
+        console.log("SOMETHING WENT WRONG");
+        console.log(error);
+    }else{
+        if(response.statusCode == 200){
+            console.log(body);
+        }
     }
 });
